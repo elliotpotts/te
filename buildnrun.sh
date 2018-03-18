33f08f36d3dbf2c613db311eb92bc7ea0bba542c
Elliot Potts <elliotpotts@gmail.com>
2018-03-18 22:47:26 +0800

    added ninja build

--- buildnrun.sh
+++ buildnrun.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-g++ -g -Wall -Werror -std=c++17 -I. -Iglm-0.9.8.5 -Iglfw-3.2.1/include -Iglad/include main.cpp glad/src/glad.c -lFreeImage -Lglfw-3.2.1/build/src -lglfw3 -lgdi32 -lopengl32 && ./a.exe
+ninja && ./a.exe
